7aa5ab802c4ee0bbb50dc839fe466a04a535b5fe
zhonguncle_pi <2020738339@qq.com>
2024-07-22 05:03:16 +0800

    fix syntax error

--- clone_rasp_disk.sh
+++ clone_rasp_disk.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 # clone Raspberry Pi system to target disk
 
-targetPath=$1
+targetDisk=$1
 
-if [[ $targetPath=="/dev/mmcblk0" ]]; then
-    target="/dev/mmcblk0p"
+if [[ $targetDisk = "/dev/mmcblk0" ]]; then
+    targetPartPrefix="/dev/mmcblk0p"
 else
-    target=$targetPath
+    targetPartPrefix=$targetDisk
 fi
 
 
-if [ $target ]; then
-    sudo parted $target --script 'mklabel msdos mkpart primary fat32 4194.5kB 541MB mkpart primary ext4 541MB 100% print quit'
+if [ $targetPartPrefix ]; then
+    sudo parted $targetDisk --script 'mklabel msdos mkpart primary fat32 4194.5kB 541MB mkpart primary ext4 541MB 100% print quit'
 
-    sudo mkfs.vfat -F 32 $target"1"
-    sudo mkfs.ext4 -L rootfs $target"2"
+    sudo mkfs.vfat -F 32 $targetPartPrefix"1"
+    sudo mkfs.ext4 -L rootfs $targetPartPrefix"2"
 
     sudo mkdir /mnt/rootfs
-    sudo mount $target"2" /mnt/rootfs/
-    sudo rsync -axHAWXS --numeric-ids --info=progress2 --exclude={"/mnt/","/boot/firmware/","/dev","/media","/proc","/sys","/lost+found","/run","/tmp"} /* /mnt/rootfs
+    sudo mount $targetPartPrefix"2" /mnt/rootfs/
+    sudo rsync -axHAWXS --numeric-ids --info=progress2 --exclude={"/mnt/","/boot/firmware/","/proc","/lost+found","/run"} /* /mnt/rootfs
+
+    mkdir /mnt/rootfs/run /mnt/rootfs/proc
 
     sudo mkdir /mnt/rootfs/mnt /mnt/rootfs/boot/firmware
-    sudo mount $target"1" /mnt/rootfs/boot/firmware/
+    sudo mount $targetPartPrefix"1" /mnt/rootfs/boot/firmware/
     sudo rsync -axHAWXS --numeric-ids --info=progress2 /boot/firmware/* /mnt/rootfs/boot/firmware/
 
-    uuid1=$(sudo lsblk $target -o PARTUUID | tail -2 | head -1)
-    uuid2=$(sudo lsblk $target -o PARTUUID | tail -2 | tail -1)
+    uuid1=$(sudo lsblk $targetDisk -o PARTUUID | tail -2 | head -1)
+    uuid2=$(sudo lsblk $targetDisk -o PARTUUID | tail -2 | tail -1)
 
     sudo sed -i -e "s/PARTUUID=........-01/PARTUUID=$uuid1/g" /mnt/rootfs/etc/fstab
     sudo sed -i -e "s/PARTUUID=........-02/PARTUUID=$uuid2/g" /mnt/rootfs/etc/fstab
